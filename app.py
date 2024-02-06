bb393f8e7c7a94b9e629418d11be01a284396e2a
Zach-Burge <zburge@zagmail.gonzaga.edu>
2024-02-06 15:58:38 -0800

    number input

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import requests
 
-st.write("# Hello World!")
+st.title("Hello World!")
+st.text_input("Enter a Number:")
+
+if st.button("Source"):
+    response = requests.get(st.secrets["endpoint"])
+    st.write(response.content)
