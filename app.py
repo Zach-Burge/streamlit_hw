06e75d6507f84e8b46f85ce16aed46fbe503fde9
Zach-Burge <zburge@zagmail.gonzaga.edu>
2024-02-13 12:02:07 -0800

    Made it a POST request

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ age = st.number_input("Enter your age:", step=1)
 
 if st.button("Enter"):
     chalice_endpoint = st.secrets["endpoint"]
-    endpoint = f"{chalice_endpoint}/{age}/{name}"
-    response = requests.get(endpoint)
+    data = {'name': name, 'age': age}
+    response = requests.post(chalice_endpoint, json=data)
     result = response.json()
-    st.title(f"{result['name']} will be {result['age_incremented']}")
+    # result_data = result['result']
+    st.title(f"{result['name']} will be {result['new_age']}")
