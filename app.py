6036dd08c470a7685242a1dbf118d78b8eedb8e7
Zach-Burge <zburge@zagmail.gonzaga.edu>
2024-02-13 15:43:46 -0800

    Added request json

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ age = st.number_input("Enter your age:", step=1)
 
 if st.button("Enter"):
     chalice_endpoint = st.secrets["endpoint"]
-    data = {'name': name, 'age': age}
+    data = {'age': age, 'name': name}
     response = requests.post(chalice_endpoint, json=data)
     result = response.json()
-    # result_data = result['result']
+    
+    # Front end formatting
     st.title(f"{result['name']} will be {result['new_age']}")
-    st.title("JSON")
+    st.title("JSON Request")
+    st.text(data)
+    st.title("JSON Response")
     st.text(result)
