d8974b35a48512cacd893d0f5dc32fcfdd169b4b
Zach-Burge <zburge@zagmail.gonzaga.edu>
2024-02-13 11:09:50 -0800

    Changed JSON response

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import streamlit as st
 import requests
 
 st.title("Hello World!")
-number = st.number_input("Enter a Number:", step=1)
+name = st.text_input("What is your name?")
+age = st.number_input("Enter your age:", step=1)
 
 if st.button("Enter"):
     chalice_endpoint = st.secrets["endpoint"]
-    endpoint = f"{chalice_endpoint}{number}"
-    response = requests.get(endpoint).json()
-    result = response['result']
-    st.title(result)
+    endpoint = f"{chalice_endpoint}/{age}/{name}"
+    response = requests.get(endpoint)
+    result = response.json()
+    st.title(f"{result['name']} will be {result['age_incremented']}")
